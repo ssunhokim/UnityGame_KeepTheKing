1fec4b96cf32edf6600e8d1993d18136ed431ac8
김선호 <algorithm887@naver.com>
2018-01-16 18:23:20 +0900

    ※ 몬스터 겹침 현상 리지드바디이용해서 좀 줄임 ◎ 자지 않기...

---
+++ Assets/Script/Monster/FSM/State_Damaged.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class State_Damaged : FSM_State<Agent>
+{
+
+    static readonly State_Damaged instance = new State_Damaged();
+    public static State_Damaged Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    private float resetTime = 3.0f;
+    private float currentTime;
+
+    static State_Damaged() { }
+
+    private State_Damaged()
+    {
+
+    }
+
+    public override void EnterState(Agent _Monster)
+    {
+        _Monster.animator.SetBool("IsDamaged", true);
+        _Monster.IsDead = true;
+    }
+    public override void UpdateState(Agent _Monster)
+    {
+        if(_Monster.CurrentHP <= 0)
+        {
+            _Monster.ChangeState(State_Die.Instance);
+            return;
+        }
+
+        if(_Monster.idleTime <= _Monster.currentIdleTime)
+        {
+            _Monster.ChangeState(State_Idle.Instance);
+            _Monster.currentIdleTime = 0.0f;
+            return;
+        }
+        else
+        {
+            _Monster.currentIdleTime += Time.deltaTime;
+        }
+    }
+
+    public override void ExitState(Agent _Monster)
+    {
+        _Monster.animator.SetBool("IsDamaged", false);
+    }
+}
